89caf2fc048eaccbbc162d3e579e3667d65d20a5
zongweiyang <yzwok@sina.cn>
2016-01-17 00:33:15 +0800

    add first controller

---
+++ src/main/java/com/test/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.test.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by yzw on 2016/1/17.
+ */
+@RestController
+public class TestController {
+
+    public Object follow(){
+        return "follow fun";
+    }
+}
