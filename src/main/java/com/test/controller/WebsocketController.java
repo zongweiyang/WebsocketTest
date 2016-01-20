2cb6677593a3e3d79a2c7d6836af65dc28c1434b
zongwei.yang <zongwei.yang@datayes.com>
2016-01-20 17:58:49 +0800

    add dockerfile

--- src/main/java/com/test/controller/WebsocketController.java
+++ src/main/java/com/test/controller/WebsocketController.java
@@ -5,6 +5,9 @@ import com.test.service.HelloMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.web.servlet.support.RequestContext;
 
 /**
  * Created by yzw on 2016/1/17.
@@ -17,8 +20,8 @@ public class WebsocketController {
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Object greeting(HelloMessage message) throws InterruptedException {
-        System.out.println("message comming");
-        Thread.sleep(3000); // simulated delay
-        return new Greeting("hello, "+ message.getName() + "!");
+        System.out.println("message comming: "+message.getName ());
+//        Thread.sleep(3000); // simulated delay
+        return new Greeting("hello, "+ message.getName());
     }
 }
